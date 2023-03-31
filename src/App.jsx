113b5901b18b4320886cd815da66ade59bd8ea9d
Fernando Guerrero <devrrior@gmail.com>
2023-03-31 09:35:25 -0600

    On disconnect go home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import CreateAnnoun from './screens/CreateAnnoun';
 import Cards from './screens/Cards';
 import Sidebar from './components/Sidebar';
@@ -10,10 +10,20 @@ import { HomePage } from './screens/HomePage';
 import { Navbar } from './Navbar';
 import Cards2 from './screens/Cards2';
 import { WebsocketProvider } from './contexts/websocket/websocketProvider';
+import { useWebsocket } from './hook/useWebsocket';
 
 function App() {
   //ESTADO MANDANDO PARA COMPONENTES HERMANOS (POR ESO ESTA EN COMPONENTE PADRE)
   const [searchText, setSearchText] = useState('');
+  const {websocket} = useWebsocket();
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    websocket.on('disconnect', () => {
+      navigate('/');
+    });
+
+  }, []);
 
   return (
     <WebsocketProvider>
